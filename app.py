4f5a01cc259b1c3b7b4a325db1c41ce8ad7d0372
Tom O'Dell <tpodell140@gmail.com>
2017-08-17 20:57:43 -0400

    Added Inventory display and add for individual dealerships

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def addDealer():
     if request.method == 'POST':
         logo = request.form['logo']
         if not logo:
-            logo = 'http://luxmimotorssirsa.com/wp-content/plugins/wp-car-manager/assets/images/placeholder-single.png'
+            logo = 'https://oncarrot.com/app/themes/oncarrot/images/property_placeholder.jpg'
         dealership = Dealership(name = request.form['name'], location = request.form['location'], make = request.form['make'], logo= logo)
         session.add(dealership)
         session.commit()
@@ -78,7 +78,36 @@ def deleteDealer(dealer_id):
 # Inventory of a specific dealership
 @app.route('/dealerships/<int:dealer_id>/cars')
 def displayInventory(dealer_id):
-    return "Page for Dealer with ID# {}".format(dealer_id)
+    dealership = session.query(Dealership).filter_by(id = dealer_id).one()
+    inventory = session.query(Car).filter_by(dealer_id = dealer_id)
+    return render_template('displayInventory.html', dealership = dealership, inventory = inventory)
+
+
+# Add a car to inventory
+@app.route('/dealership/<int:dealer_id>/cars/addCar', methods = ['GET', 'POST'])
+def addCar(dealer_id):
+    dealership = session.query(Dealership).filter_by(id = dealer_id).one()
+    if request.method == 'POST':
+        image = request.form['image']
+        if not image:
+            image = 'http://luxmimotorssirsa.com/wp-content/plugins/wp-car-manager/assets/images/placeholder-single.png'
+        newCar = Car(
+                    status = request.form['status'],
+                    make = request.form['make'],
+                    model = request.form['model'],
+                    year = request.form['year'],
+                    price = request.form['price'],
+                    image = image,
+                    mileage = request.form['mileage'],
+                    color = request.form['color'],
+                    dealer_id = dealership.id
+                    )
+        session.add(newCar)
+        session.commit()
+        return redirect(url_for('displayInventory', dealer_id = dealership.id))
+    if request.method == 'GET':
+        return render_template('addCar.html', dealership = dealership)
+
 
 
 # Information for a specific car

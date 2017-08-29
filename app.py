78a5b4fb635baf81df872bc4c63a18fbb178d6a9
Tom O'Dell <tpodell140@gmail.com>
2017-08-29 15:55:54 -0400

    Added oAuth login functionality

--- app.py
+++ app.py
@@ -1,27 +1,154 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, request, redirect, jsonify, url_for
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 app = Flask(__name__)
 
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from models import Base, Dealership, Car
+from models import Base, Dealership, Car, User
 
 from flask import session as login_session
 import random, string
 
-# oAuth import statements
+from oauth2client import client, crypt
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
 import httplib2
+import json
 from flask import make_response
 import requests
 
-
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
 # Connect to database and create session
 engine = create_engine('sqlite:///dealerinventory.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
+
+
+# Create a state token to prevent request forgery.
+# Store it in the session for later validation.
+@app.route('/login/')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.
+        digits) for x in range(32))
+    login_session['state'] = state
+    return render_template('login.html', STATE=state)
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    code = request.data
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json',scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(json.dumps('Failed to upgrade the authorization code'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    #Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+
+    #Verify that the access token is used for the intended user:
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    #Verify that the access token is valid for this app:
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(json.dumps("Token's client ID doesn't match given client ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    #Check to see if user is already logged in:
+    stored_credentials = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps("Current user is already connected"), 200)
+        response.headers['Content-Type'] = 'application/json'
+
+    #Store the access tokens in the session for later use:
+    login_session['access_token'] = access_token
+    login_session['gplus_id'] = gplus_id
+
+    #Get user info:
+    userinfo_url = "https://www.googleapis.com/userinfo/v2/me"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+    data = json.loads(answer.text)
+
+    login_session['username'] = data["name"]
+    login_session['picture'] = data["picture"]
+    print(data)
+    login_session['email'] = data["email"]
+    login_session['provider'] = 'google'
+
+
+    # See if user exists, if not create new user
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print ("done!")
+    return output
+
+
+
+@app.route('/gdisconnect/')
+def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print ('Access Token is None')
+        response = make_response(json.dumps('Current user is not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    print ('In gdisconnect access token is %s', access_token)
+    print ('User name is: ')
+    print (login_session['username'])
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print ('result is ')
+    print (result)
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user'), 400)
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 
 # Main inventory listing
@@ -149,7 +276,50 @@ def deleteCar(dealer_id, car_id):
         return render_template('deleteCar.html', dealership = dealership, car = car)
 
 
+# JSON endpoints
+@app.route('/dealerships/JSON')
+def dealershipJSON():
+    dealerships = session.query(Dealership).all()
+    return jsonify(dealerships = [dealer.serialize for dealer in dealerships])
+
+
+@app.route('/dealerships/<int:dealer_id>/cars/JSON')
+def dealerInventoryJSON(dealer_id):
+    cars = session.query(Car).filter_by(dealer_id = dealer_id).all()
+    return jsonify(cars = [car.serialize for car in cars])
+
+
+@app.route('/dealerships/<int:dealer_id>/cars/<int:car_id>/JSON')
+def carJSON(dealer_id, car_id):
+    car = session.query(Car).filter_by(id = car_id).one()
+    return jsonify(car = car.serialize)
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email = email).one()
+        return user.id
+    except:
+        return None
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id = user_id).one()
+    return user
+
+
+
+def createUser(login_session):
+    newUser  = User(name =login_session['username'],
+        email = login_session['email'], picture = login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email = login_session['email']).one()
+    return user.id
+
+
 
 if __name__ == '__main__':
+    app.secret_key = "Super_secret_key"
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
--- models.py
+++ models.py
@@ -7,6 +7,25 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
+class User(Base):
+    __tablename__ = 'user'
+
+    name = Column(String(250), nullable = False)
+    email = Column(String(250), nullable = False)
+    picture = Column(String(250))
+    id = Column(Integer, primary_key = True)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'name'      : self.name,
+            'email'     : self.email,
+            'picture'   : self.picture,
+            'id'        : self.id
+        }
+
 class Dealership(Base):
     __tablename__ = 'dealership'
 
@@ -15,6 +34,17 @@ class Dealership(Base):
     logo = Column(String(250))
     make = Column(String(50), nullable = False)
     id = Column(Integer, primary_key = True)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'name'      : self.name,
+            'location'  : self.location,
+            'logo'      : self.logo,
+            'make'      : self.make,
+            'id'        : self.id
+        }
 
 class Car(Base):
     __tablename__ = 'car'
@@ -30,6 +60,22 @@ class Car(Base):
     id = Column(Integer, primary_key= True)
     dealer_id = Column(Integer, ForeignKey('dealership.id'))
     dealership = relationship(Dealership)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'make'      : self.make,
+            'model'     : self.model,
+            'year'      : self.year,
+            'status'    : self.status,
+            'price'     : self.price,
+            'image'     : self.image,
+            'mileage'   : self.mileage,
+            'color'     : self.color,
+            'id'        : self.id,
+            'dealer_id' : self.dealer_id
+        }
 
 engine = create_engine('sqlite:///dealerinventory.db')
 
